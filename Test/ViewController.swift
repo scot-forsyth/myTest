ac4b6a3acc1e7007010ccccf7edff901389874a6
Scot Forsyth <Scot@scots-imac.local>
2015-10-19 13:03:29 +0100

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Scot Forsyth on 19/10/2015.
+//  Copyright © 2015 Scot Forsyth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
