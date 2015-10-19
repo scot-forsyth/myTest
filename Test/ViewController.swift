ba8948f9961ed3b9d4b3003fb00069d33c809a7d
Scot Forsyth <scot.forsyth@myunidays.com>
2015-10-19 13:34:09 +0100

    My first fun

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /**
+    Test method for a certain thing
+    */
+    func testMethod(){
+        
+    }
 
 }
 
